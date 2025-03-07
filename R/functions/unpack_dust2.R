e1e140209fbfae519e2cfab41c756992fb71dc59
arranhamlet <arran.hamlet14@imperial.ac.uk>
2025-03-07 13:22:21 +0000

    Added in function to clean outputs for plotting

---
+++ R/functions/unpack_dust2.R
@@ -0,0 +1,53 @@
+
+unpack_dust2 <- function(model_system, model_object, dimension_names){
+  
+  #Unpack state to lists
+  dust_state <- dust_unpack_state(model_system, model_object)
+  
+  #Work out which are compartments and which are not
+  these_are_compartments <- sapply(model_system$packing_state, function(x) length(x) != 0)
+  
+  #Unpack all the compartments
+  unpacked_compartments <- data.table::rbindlist(sapply(as.numeric(which(these_are_compartments)), function(x){
+    
+    print(x)
+    
+    #Unpack this object
+    this_obj <- dust_state[[x]]
+    
+    #Loop through dimnames
+    for(i in length(dimension_names)){
+      dimnames(this_obj)[[i]] <- unlist(dimension_names[[i]])
+    }
+    
+    melted_array <- reshape2::melt(this_obj) %>%
+      set_names(c(names(dimension_names), "value")) %>%
+      mutate(state = names(dust_state)[x]) %>%
+      select(time, state, names(dimension_names), value)
+    
+    melted_array
+    
+  }, simplify = FALSE))
+  
+  #Add in the non-compartment outputs
+  unpacked_noncompartments <- data.table::rbindlist(sapply(as.numeric(which(!these_are_compartments)), function(x){
+    
+    #Unpack this object
+    this_obj <- dust_state[[x]]
+    
+    #Loop through dimnames
+    data.frame(this_obj) %>% 
+      set_names("value") %>%
+      mutate(time = dimension_names[[which(names(dimension_names) == "time")]][[1]]) %>%
+      mutate(state = names(dust_state)[x])
+    
+  }, simplify = FALSE))
+  
+  #Combine and spit out
+  plyr::rbind.fill(
+    unpacked_compartments, 
+    unpacked_noncompartments
+    ) %>%
+    mutate(state = factor(state, levels = names(dust_state)))
+  
+}
--- R/testing/run_models.R
+++ R/testing/run_models.R
@@ -12,54 +12,49 @@ model <- odin2::odin("models/mini_model.R")
 
 #Set up parameters
 pars <- list(
-  R0 = 1.1,
-  N0 = matrix(1000, nrow = 1, ncol = 1),
-  I0 = matrix(1, nrow = 1, ncol = 1),
+  
+  #Dimensions
+  n_age = 1,#2,
+  n_vacc = 1,
+  
+  #Single dimension parameters
+  R0 = 1.5,
   recovery_rate = 1/14,
   incubation_rate = 1/5,
-  b = 0,#1/(8 * 365),
+  b = 0,
   severe_recovery_rate = 1/14,
-  prop_severe = matrix(0, nrow = 1, ncol = 1),
   prop_complications = 0.1,
-  age_vaccination_beta_modifier = matrix(1, nrow = 1, ncol = 1)
+  
+  #Multi dimension parameters
+  N0 = matrix(1000),#matrix(c(750, 250), nrow = 2, ncol = 1),
+  I0 = matrix(1), #matrix(c(1, 0), nrow = 2, ncol = 1),
+  prop_severe = matrix(0.1),#matrix(c(0.1, 0.01), nrow = 2, ncol = 1),
+  age_vaccination_beta_modifier = matrix(1)#matrix(c(1, 0.5), nrow = 2, ncol = 1)
+  
 )
 
 #Define dust system and initialise
 sys <- dust2::dust_system_create(model(), pars)
 dust2::dust_system_set_state_initial(sys)
 
-#Set time
-time <- 0:400
-#Run model
+#Set time and run model
+time <- 0:700
 y <- dust2::dust_system_simulate(sys, time)
-state <- dust_unpack_state(sys, y)
 
-output <- matrix(
-  unlist(state), ncol = length(state), byrow = F
-  ) %>%
-  as.data.frame() %>%
-  set_names(
-    names(state)
-    ) %>%
-  rownames_to_column(
-    var = "time"
-    ) %>%
-  mutate(
-    time = as.numeric(time)
-  ) %>%
-  gather(
-    key = "key",
-         value = "value",
-         -time
-    ) %>%
-  mutate(
-    key = factor(key, levels = names(state))
+#Unpack columns
+clean_df <- unpack_dust2(
+  model_system = sys, 
+  model_object = y, 
+  dimension_names = dimension_names <- list(
+    age = list("Child", "Adult"), 
+    vaccination_status = "Unvaccinated", 
+    time = list(0:700)
   )
-
+)
 
 #Plot
 ggplot(
-  data = output,
+  data = clean_df,
   mapping = aes(
     x = time,
     y = value
@@ -67,9 +62,11 @@ ggplot(
 ) +
   geom_point() +
   facet_wrap(
-    ~key,
+    ~state,
     scales = "free_y"
   )
+
+
 
 
 
